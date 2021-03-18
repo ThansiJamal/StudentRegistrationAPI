11d0a0433fe4310e3a4fbc70a40dd8d7ea005e67
ThansiJamal <thansijamal18@gmail.com>
2021-03-17 21:21:10 -0700

    first commit

---
+++ StudentRegistrationAPI/Controllers/Student2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentRegistrationAPI.Controllers
+{
+    public class Student2Controller : Controller
+    {
+        // GET: Student2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
